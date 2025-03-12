dd1e13f8ae3c4144e640b3727aa62086c6274389
Joy Mondal <jaymondals953@gmail.com>
2025-03-13 00:27:48 +0530

    Delete js directory

--- js/mouse_scroll.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector(".navbar-container");
-
-    navbar.addEventListener("wheel", function (event) {
-        event.preventDefault();
-        navbar.scrollLeft += event.deltaY; // Scroll horizontally using vertical wheel scroll
-    });
-});
